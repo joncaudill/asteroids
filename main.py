eed354a627849317c756688822d9e803354a2ef0
joncaudill <jon.caudill@outlook.com>
2024-12-10 21:45:48 -0500

    completed draw player assignment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
@@ -7,12 +8,18 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	game_clock = pygame.time.Clock()
+	dt = 0
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 	while(True):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		pygame.Surface.fill(screen, (0,0,0))
+		pygame.Surface.fill(screen, "black")
+		player.draw(screen)
 		pygame.display.flip()
+		delta_time = game_clock.tick(60)
+		dt = delta_time / 1000
 
 if __name__ == "__main__":
 	main()
