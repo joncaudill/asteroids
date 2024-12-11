513f1638b5b23bd7590b0d147587e112991e66ef
joncaudill <jon.caudill@outlook.com>
2024-12-10 22:34:56 -0500

    created pygame groups and switched game loop to groups

--- main.py
+++ main.py
@@ -10,14 +10,22 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	game_clock = pygame.time.Clock()
 	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 	while(True):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		pygame.Surface.fill(screen, "black")
-		player.update(dt)
-		player.draw(screen)
+		
+		for thing in updatable:
+			thing.update(dt)
+
+		for thing in drawable:
+			thing.draw(screen)
+		
 		pygame.display.flip()
 		delta_time = game_clock.tick(60)
 		dt = delta_time / 1000
