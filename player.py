522630f62f1a1ce39a8d20c4348b9fc9b2889e12
joncaudill <jon.caudill@outlook.com>
2024-12-10 23:59:13 -0500

    enforced ratelimit on shoot method

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN
 
     def draw(self,screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -19,10 +20,14 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position[0], self.position[1])
-        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.shot_cooldown_timer <= 0:    
+            shot = Shot(self.position[0], self.position[1])
+            shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN
 
     def update(self, dt):
+        self.shot_cooldown_timer -= dt
+        
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
